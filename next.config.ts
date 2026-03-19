0b4c00d8131e7832697391409f5b72cfe4a0204f
Thorsten Schminkel <thorsten@schminkel.de>
2026-03-19 08:37:31 +0100

    fix: update connect-src to conditionally allow localhost in development

--- next.config.ts
+++ next.config.ts
@@ -48,7 +48,7 @@ const securityHeaders = [
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://hcaptcha.com https://*.hcaptcha.com",
       "img-src 'self' data: blob: https://lh3.googleusercontent.com https://*.googleusercontent.com https://hcaptcha.com https://*.hcaptcha.com https://www.googletagmanager.com https://www.google-analytics.com",
       "font-src 'self' data: https://fonts.gstatic.com",
-      "connect-src 'self' https://*.supabase.co wss://*.supabase.co https://generativelanguage.googleapis.com https://hcaptcha.com https://*.hcaptcha.com https://www.googletagmanager.com https://www.google-analytics.com https://region1.google-analytics.com https://us.i.posthog.com https://*.posthog.com",
+      `connect-src 'self' https://*.supabase.co wss://*.supabase.co https://generativelanguage.googleapis.com https://hcaptcha.com https://*.hcaptcha.com https://www.googletagmanager.com https://www.google-analytics.com https://region1.google-analytics.com https://us.i.posthog.com https://*.posthog.com${isDev ? ' http://localhost:8000' : ''}`,
       "frame-src 'self' https://hcaptcha.com https://*.hcaptcha.com",
       "frame-ancestors 'self'",
       "base-uri 'self'",
